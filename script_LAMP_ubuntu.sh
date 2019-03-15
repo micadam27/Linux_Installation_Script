07b33baeaf38cea49baa2185ff498628df7d913b
micadam27 <micadam27@hotmail.com>
2019-03-15 11:13:07 -0400

    Update script_LAMP_ubuntu.sh

--- script_LAMP_ubuntu.sh
+++ script_LAMP_ubuntu.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install -y apache2
 apt-get install -y mysql-server
-apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
+apt-get install -y php libapache2-mod-php php-mysql
 mysql_secure_installation
